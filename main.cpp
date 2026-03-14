701484c659d49dc1823134d24fb265338b23990c
Daniel <dhaddad3@students.avc.edu>
2026-03-13 19:56:52 -0700

    commit 1

--- main.cpp
+++ main.cpp
@@ -9,15 +9,16 @@ int main()
     int rdnum1, rdnum2, rdnum3;
     int total = 0;
 
-    // TODO: seed the random number generator with time(0)
+    srand(static_cast<unsigned int>(time(0)));
 
-    // TODO: generate 3 random numbers between 0 and 99
+    rdnum1 = rand() % 100;
+    rdnum2 = rand() % 100;
+    rdnum3 = rand() % 100;
+    total = rdnum1 + rdnum2 + rdnum3;
 
-    // TODO: compute the total summation
+    cout << rdnum1 << " " << rdnum2 << " " << rdnum3 << endl;
 
-    // TODO: print the 3 random numbers on the first line
-
-    // TODO: print the total and average (integer division) on the second line
+    cout << total << " " << (total / 3) << endl;
 
     return 0;
 }
